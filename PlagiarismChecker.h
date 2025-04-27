d15d084e5ba66b183835da14555d0d42cfbc3d21
rewp <artemblinov480@gmail.com>
2025-04-27 22:49:59 +0300

    add slots

--- PlagiarismChecker.h
+++ PlagiarismChecker.h
@@ -1,4 +1,23 @@
 #ifndef PLAGIARISMCHECKER_H
 #define PLAGIARISMCHECKER_H
 
+#include <vector>
+#include <string>
+#include <utility>
+
+class PlagiarismChecker
+{
+public:
+    PlagiarismChecker();
+    ~PlagiarismChecker();
+
+    std::vector<std::pair<std::string, double>> checkPlagiarism(const std::string &inputText) const;
+
+private:
+    std::vector<std::pair<std::string, std::string>> dataset;
+
+    std::vector<std::string> tokenizeString(const std::string &text) const;
+    double computeJaccard(const std::vector<std::string> &tokens1,
+                          const std::vector<std::string> &tokens2) const;
+};
 #endif // PLAGIARISMCHECKER_H
